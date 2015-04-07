8d1721dae0964e4d59666a505aba186b77ec8ea2
John Furr <jfurr@Johns-MacBook-Pro.local>
2015-04-07 14:53:42 -0400

    Random commits to clean up the tree

--- Scripts/LoadMol.py
+++ Scripts/LoadMol.py
@@ -7,57 +7,53 @@ from ReadMolecule import *
 #import random
 
 def LoadMol(fileName):
-	print "Filename: " + str(fileName)
-	View = molView()
-	if not View:
-		print "View was not initizlised"
+    print "----------LoadMol-------------"
+    print "Filename: " + str(fileName)
+    View = molView()
+    if not View:
+        print "View was not initizlised"
 
-	ext = fileName[-3:]
-	reader = Reader()
-	mol = ""
-	if ext == 'sdf' or ext ==  'mol':
-		numMols = reader.scanSDF(fileName)
-		index = 0;
-		while index < numMols:
-			mol = reader.getSDF(fileName, 0)
-			print "Read mol -?- : " + mol.name
-			index = index + 1
-			View.Show(mol)
-			#time.sleep(0.5)	
-	if ext == 'pdb':
+    ext = fileName[-3:]
+    reader = Reader()
+    mol = ""
+    if ext == 'sdf' or ext ==  'mol':
+        numMols = reader.scanSDF(fileName)
+        index = 0;
+        while index < numMols:
+            mol = reader.getSDF(fileName, 0)
+            print "Read mol -?- : " + mol.name
+            index = index + 1
+            View.Show(mol)
+    if ext == 'pdb':
 		
-		numMols = reader.scanPDB(fileName)
-		index = 0
-		while index < 1:
-		#while index < numMols:
-			mol = reader.getPDBModel(fileName, index)
-			index = index + 1
+        numMols = reader.scanPDB(fileName)
+        index = 0
+        while index < 1:
+            mol = reader.getPDBModel(fileName, index)
+            index = index + 1
 			
-			#raw_input()
-		View.Show(mol)
-		del(sys.modules['PDBRead'])
-		del(sys.modules['ReadMolecule'])
-		#raw_input()	
+        View.Show(mol)
+        del(sys.modules['PDBRead'])
+        del(sys.modules['ReadMolecule'])
 
-	## This might be good information to save so you can retrive molecules from the file
-	#FileOffSets = reader.sdread.recIndex
+    ## This might be good information to save so you can retrive molecules from the file
+    #FileOffSets = reader.sdread.recIndex
 
-	#for i in FileOffSets:
-	#	print i
+    #for i in FileOffSets:
+    #	print i
 
-	#View.Show(mol)
+    #View.Show(mol)
 
-	return mol
+    return mol
 
 if __name__ == '__main__':
-	print "You are in the LoadMol.py main function"
-	mol = ""
-	if len(sys.argv) > 1:
-		
-		mol = LoadMol(sys.argv[1])
-	else:
-		mol = LoadMol("test.sdf")
+    print "You are in the LoadMol.py main function"
+    mol = ""
+    if len(sys.argv) > 1:
+        mol = LoadMol(sys.argv[1])
+    else:
+        mol = LoadMol("test.sdf")
 
-	print mol.name
-	print "Num atoms: " + str(len(mol.atoms))
-	print "Num bonds: " + str(len(mol.bonds))
+    print mol.name
+    print "Num atoms: " + str(len(mol.atoms))
+    print "Num bonds: " + str(len(mol.bonds))
--- python_extensions/Molecule/test.py
+++ python_extensions/Molecule/test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #import molecule
 from molecule import *
+from ctypes import c_int, addressof
 
 print "Test 1.  Molecule and Atom Object Creation"
 m = Mol()
@@ -8,20 +9,33 @@ atm1 = m.addAtom(1, 'N1', 0, 0,0, 7)
 atm2 = m.addAtom(2, 'C2', 0, 1, 0, 6)
 atm3 = m.addAtom(3, 'C3', 0, 0, 1, 6)
 
+#for a in m.atoms:
+#    print "a: ", a, a.symbol, a.parent()
+print "-------------------------------------------"
+print "m.name: ", m.name
+print "atm1: %s, %s "%( atm1.symbol,  atm1.parent())
+print "atm2: %s, %s "%(atm2.symbol,  atm2.parent())
+print "atm3: %s, %s"%(atm3.symbol,  atm3.parent())
+raw_input()
+atm = m.atoms[0]
+print "atm: ", atm, atm.parent(), m.atoms[0]
+
+
 print "\n\nTest 2.  Molecule and Atom Object chages"
 
 print "atm1.symbol: " + atm1.symbol
 atm1.symbol = "hello"
 m.name = "Molecule Has A Test Name"
 
-print "\n\nTest 3.  Using a for i in m.atoms syntax"
-for i in m.atoms:
-	print str(i.num) + "\t" + i.symbol
+#print "\n\nTest 3.  Using a for i in m.atoms syntax"
+#for i in m.atoms:
+#	print str(i.num) + "\t" + i.symbol
 
 print "\n\nTest 4.  Checking atom parent"
 atm = m.atoms[0]
 par = atm.parent()
-print par.name
+print "par: ", par
+raw_input()
 
 m2 = Mol()
 m2.name = "Molecule 2"
