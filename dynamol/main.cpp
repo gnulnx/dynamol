eb4f27ea1cba7f0332df98fe7a8e44cca4b35eaa
John Furr <jfurr@Johns-MacBook-Pro.local>
2015-04-08 09:24:57 -0400

    cleaned up main.cpp

--- dynamol/main.cpp
+++ dynamol/main.cpp
@@ -1,7 +1,3 @@
-//#include <boost/python.hpp>
-//#include <boost/detail/lightweight_test.hpp>
-
-
 #include <Qt>
 #include <QApplication>
 
@@ -34,6 +30,7 @@ using dynamol::script_molView;
 using dynamol::ScriptEngine;
 
 molecule *readMol2(string fileName) {
+    //TODO This needs to be offloaded to python code.   
 	string name = "None";
 	molecule *mol = new molecule(name);
 	ifstream inFile(fileName.c_str(), ios::in);
@@ -53,21 +50,15 @@ molecule *readMol2(string fileName) {
 	string symbol, mol2_type;
 	float x, y, z;
 	for (int i=0; i<numAtm; i++) {
-		//cout << i << endl;
 		inFile >> num >> symbol >> x >> y >> z >>mol2_type;
 		getline(inFile, tmp);
 		mol->addAtom(num, symbol, x, y, z);		
 	}
-	//cout <<"Now Reading Bonds"<<endl;
 	getline(inFile, tmp);
 	int bdNum, fromNum, toNum, order;
 	string sorder;
 	for (int i=0; i<numBd; i++) {
 		inFile >> bdNum >> fromNum >> toNum >> sorder;
-		//getline(inFile, tmp);
-		//cout << bdNum <<"\t" << fromNum << endl;
-		//cout <<"bond tmp: " << tmp << endl;
-		//cin >> test;
 		if (sorder == "1")
 			order = 1;	
 		if (sorder == "ar")
@@ -77,8 +68,6 @@ molecule *readMol2(string fileName) {
 		if (sorder == "3")
 			order = 3;
 		
-		//order = 1;
-		//cout << bdNum <<"\t"<<fromNum<<"\t"<<toNum<<"\t"<<sorder<<endl;
 		bond *bd = mol->addBond(i+1, fromNum, toNum, order);
 		
 	}
@@ -88,24 +77,11 @@ molecule *readMol2(string fileName) {
 };
 
 int main(int argc, char *argv[]) {
-
-
+    //NOTE: Rename this to molfile or something since you 
+    //      will determine the type in python code and then load the molecule
 	string sdFile = argv[1];
 	QApplication a(argc, argv);
-	//molecule *mol = readSD("SDF/Trans.sdf");
-	//molecule *mol = readMol2("SDF/120939.mol2");
-	//molecule *mol = readMol2("SDF/1GW7.mol2");
-    /*
-	molecule *mol;
-	if (sdFile[ sdFile.size()-1 ] == 'f') {
-		mol = readSD(sdFile);
-    }
-	if (sdFile[ sdFile.size()-1 ] == '2') {
-		mol = readMol2(sdFile);
-    }
-    */
-	//molecule *mol = readMol2("SDF/Really_Big_Structure.mol2");
-	//cout <<mol->atoms.size() <<"\t" << mol->bonds.size() << endl;
+
 	//Initialize the molecule viewer
 	Ui::Form ui;
 	QWidget *widget = new QWidget;
@@ -125,17 +101,11 @@ int main(int argc, char *argv[]) {
 	v2->SetScripter(scripter);
 	script_molView *mvscript = new script_molView(v2);
     
-    cout <<"script_molView: " << mvscript << endl; 
-    //int test = 0;
-    //cin >> test;
-
-	
 	molecule *mol;
 	if (sdFile[ sdFile.size()-1 ] == '2'){
     	mol = readMol2(sdFile);
 		v2->Show(mol);
 	} else {
-        cout <<"main calling SE->LoadMol"<<endl;
 		SE->LoadMol(sdFile);
 	}
 
